b8023dafb6d5314dd1d56b2dfb3568f36e72dd23
Arif <aisabuz@gmail.com>
2022-07-16 23:42:50 +0600

    Console application dependency injection added

--- EfCorePerformance/EfCorePerformance.ConsoleApp/Program.cs
+++ EfCorePerformance/EfCorePerformance.ConsoleApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EfCorePerformance.Application.Contacts;
+using EfCorePerformance.Application.Persistence;
+using EfCorePerformance.Application.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddDbContextPool<SalesDbContext>(options =>
+            {
+                // You can also use SQL Server.
+                options.UseSqlServer("data source=.;initial catalog=salesdb;integrated security=True;multipleactiveresultsets=True;");
+
+#if DEBUG
+                // Most project shouldn't expose sensitive data, which is why we are
+                // limiting to be available only in DEBUG mode.
+                // If this is not, SQL "parameters" will be '?' instead of actual values.
+                options.EnableSensitiveDataLogging();
+#endif
+            })
+                .AddTransient<IExamplesAppService, ExamplesAppService>()
+                .AddTransient<IExamplesJoinAppService, ExamplesJoinAppService>()
+                .AddTransient<IExamplesPaginationsAppService, ExamplesPaginationsAppService>()
+                .AddTransient<ITestAppService, TestAppService>()
+            .BuildServiceProvider();
+
+        //do the actual work here
+        var testService = serviceProvider.GetService<ITestAppService>();
+        var result = testService.NoTracking();
+
+
+    }
+
+}
